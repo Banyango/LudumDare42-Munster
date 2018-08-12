57fe721774af78aa8d92e280c3dae313e461847a
Kyle Reczek <kyle@banyango.com>
2018-08-12 15:08:09 -0600

    Source Files for Munster entry

---
+++ Ball.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+	[SerializeField] private Rigidbody2D _rigidbody2D;
+	[SerializeField] private AudioSource _audioSource;
+	[SerializeField] private AudioSource _audioSource2;
+	[SerializeField] private AudioClip _laughingBoy;
+	[SerializeField] private AudioClip _laughingGirl;
+	[SerializeField] private AudioClip _sadGirl;
+	[SerializeField] private AudioClip _ballHitSound;
+	[SerializeField] private Vector2 _force;
+
+
+	public Camera CameraToCheckWorldPosWith;
+	public bool IsAlone { get; set; }
+	public bool IsSad { get; set; }
+
+	public bool IsInSun
+	{
+		get { return _isInTheSun; }
+	}
+
+	private bool _isInTheSun;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		_isInTheSun = true;
+	}
+
+	private void OnTriggerStay2D(Collider2D other)
+	{
+		_isInTheSun = true;
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		_isInTheSun = false;
+	}
+
+	private void OnMouseUp()
+	{
+		if (_rigidbody2D.velocity.magnitude < 0.2f)
+		{
+
+			if (IsAlone && !_isInTheSun)
+			{
+				GirlHitsBall();
+			} else if (IsAlone)
+			{								
+				_audioSource.clip = _sadGirl;
+
+				_audioSource.Play();				
+			}
+		
+			if (!IsAlone)
+			{
+				if (_isInTheSun)
+				{
+					BoyHitsBall();
+				}
+				else
+				{
+					GirlHitsBall();
+				}
+			}
+		}		
+	}
+
+	private void BoyHitsBall()
+	{
+		HitBall(_laughingBoy);
+	}
+
+	private void GirlHitsBall()
+	{
+		if (IsSad)
+		{
+			HitBall(_sadGirl);			
+		}
+		else
+		{
+			HitBall(_laughingGirl);						
+		} 
+		
+	}
+	
+	private void HitBall(AudioClip clip)
+	{
+		
+		var ballWorldPosition = CameraToCheckWorldPosWith.WorldToScreenPoint(transform.position);
+		
+		_rigidbody2D.AddForce((ballWorldPosition - Input.mousePosition).normalized * Random.RandomRange(_force.x, _force.y), ForceMode2D.Impulse);
+
+		_audioSource.clip = clip;
+
+		_audioSource.Play();
+	}
+}
---
+++ Game.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector _houseIntro;
+    [SerializeField] private PlayableDirector _boyScene;
+    [SerializeField] private PlayableDirector _endScene;
+    [SerializeField] private PlayableDirector _theEnd;
+    [SerializeField] private Camera _menuCamera;
+    [SerializeField] private Camera _ballScene;
+    [SerializeField] private Camera _ballSceneSad;
+    [SerializeField] private Ball _ball;
+    [SerializeField] private Animator _ballAnimator;
+
+    public void Start()
+    {
+        _menuCamera.enabled = true;
+
+        StartCoroutine(GameBlah());
+        
+    }
+
+    public IEnumerator GameBlah()
+    {
+        while (!Input.anyKey)
+        {
+            yield return null;
+        }
+		
+        _menuCamera.enabled = false;
+		
+        yield return new WaitForSeconds(1f);
+		
+        // play intro
+        _houseIntro.Play();
+
+        while (_houseIntro.state == PlayState.Playing)
+        {
+            yield return null;
+        }
+		
+        _houseIntro.Stop();
+		
+        yield return new WaitForSeconds(1f);
+		
+        // ball game one
+        _ballAnimator.Play("bedroom_1");
+        _ballScene.gameObject.SetActive(true);
+
+        _ball.IsAlone = true;
+        _ball.CameraToCheckWorldPosWith = _ballScene;
+
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(10);
+            _ballAnimator.SetTrigger("Next");			
+        }
+        
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(10);
+            _ballAnimator.SetTrigger("Back");			
+        }
+
+
+        _ballScene.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(1f);
+        		
+        // boy enters house scene.
+        
+        _boyScene.Play();
+				
+        while (_boyScene.state == PlayState.Playing)
+        {
+            yield return null;
+        }
+		
+        _boyScene.Stop();
+        
+        // ball game sad
+		
+        yield return new WaitForSeconds(1f);
+		
+        _ballAnimator.Play("bedroom_1");
+        _ballSceneSad.gameObject.SetActive(true);
+
+        _ball.IsAlone = true;
+        _ball.IsSad = true;
+        _ball.CameraToCheckWorldPosWith = _ballSceneSad;
+
+        for (int i = 0; i < 4; i++)
+        {
+            yield return new WaitForSeconds(10);
+            _ballAnimator.SetTrigger("Next");			
+        }
+
+        _ballSceneSad.gameObject.SetActive(false);
+		
+        _endScene.Play();
+		
+        while (_endScene.state == PlayState.Playing)
+        {
+            yield return null;
+        }
+		
+        _endScene.Stop();
+
+        _ball.IsAlone = false;
+        _ball.IsSad = false;
+        _ball.CameraToCheckWorldPosWith = _ballScene;
+
+        _ballAnimator.Play("bedroom_4");
+        _ballScene.gameObject.SetActive(true);
+        
+        yield return new WaitForSeconds(40f);
+                
+        _theEnd.Play();
+        
+    } 
+}
